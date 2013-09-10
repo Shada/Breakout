b38c2641e103e0968d3a27b9cefd6ff75aeb6f7e
Fredrik Johannesson <fredrik.johannesson@hotmail.com>
2013-09-10 14:05:25 +0200

    Added Brick and Pad classes

---
+++ Code/Brick.h
@@ -0,0 +1,16 @@
+#ifdef _BRICK_H_
+#define _BRICK_H_
+
+#include "Object.h"
+
+namespace Logic
+{
+	class Brick: public Object
+	{
+	public:
+		Brick() : Object();
+		~Brick();
+	private:
+	}
+}
+#endif
